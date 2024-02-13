7b4fcca52713c66fecfc706c35168caf8f395c32
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-02-13 14:46:17 +0000

    .

--- index.js
+++
@@ -1,8 +0,0 @@
-// generate a random color
-const randomColor = function(){
-    const hex = '01234567890ABCDEF'
-    let color ='#'
-    for(let i=0 ; i < 6; i++){
-        color +=hex[Math.floor(Math.random()*16)]
-    }
-}
