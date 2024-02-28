c277f14b1ec61b2e74b4e21930a6cbf6b2d317ea
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-02-28 11:53:20 +0000

    asdgfdfg

--- Advance_one/promises.js
+++ Advance_one/promises.js
@@ -1,33 +1,45 @@
 const promiseOne = new Promise(function(resolve, reject){
 
-// DO an async task
-// DB calls, cyrptography , network
-setTimeout(function(){
-    console.log(`Async task is compelete`);
-    resolve()
-},1000)
-})
-
-promiseOne.then(function(){
-    console.log("Promise consumed");//  jab hmm resolve function ko call karte h tab vo then se connect hota h 
-
-})
-
-new Promise(function(resolve, reject){ // this is second promise
+    // DO an async task
+    // DB calls, cyrptography , network
     setTimeout(function(){
-        console.log("Async task 2 ");
+        console.log(`Async task is compelete`);
         resolve()
     },1000)
-}).then(function(){
-    console.log('Async 2 reolved');
-})
-
-const promiseThree = new promise(function(resolve, reject){
-    setTimeout(function(){
-        resolve({username : "Mahendra", email : "mahendra@gamil.com"})
-    },1000)
-})
-
-promiseThree.then(function(user){
-    console.log(user);
-})
+    })
+    
+    promiseOne.then(function(){
+        console.log("Promise consumed");//  jab hmm resolve function ko call karte h tab vo then se connect hota h 
+    
+    })
+    
+    new Promise(function(resolve, reject){ // this is second promise
+        setTimeout(function(){
+            console.log("Async task 2 ");
+            resolve()
+        },1000)
+    }).then(function(){
+        console.log('Async 2 reolved');
+    })
+    
+    const promiseThree = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            resolve({username : "Mahendra", email : "mahendra@gamil.com"})
+        },1000)
+    })
+    
+    promiseThree.then(function(user){
+        console.log(user);
+    })
+    
+    const promiseFour = new Promise(function(resolve, reject){
+        setTimeout(function(){
+            let error = false; // Changed to false to demonstrate resolve case
+            if(!error){
+                resolve({username : "MAHENDRA" , password: "123"})
+            } else {
+                reject ('ERROR : somthing went wrong')
+            }
+        },1000)
+    })
+    
