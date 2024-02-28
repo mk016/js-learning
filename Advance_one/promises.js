554038638ada5bfbfc934960aa2d15a111e67e1b
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-02-28 11:24:04 +0000

    adsf

--- Advance_one/promises.js
+++ Advance_one/promises.js
@@ -1,4 +1,14 @@
 const promiseOne = new Promise(function(resolve, reject){
-    
+
+// DO an async task
+// DB calls, cyrptography , network
+setTimeout(function(){
+    console.log(`Async task is compelete`);
+    resolve()
+},1000)
+})
+
+promiseOne.then(function(){
+    console.log("Promise consumed");//  jab hmm resolve function ko call karte h tab vo then se connect hota h 
 
 })
