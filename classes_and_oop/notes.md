d69f0096eeb5d18e9fd09487664ac7723ab856fd
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-02-28 13:00:24 +0000

    dfasf

---
+++ classes_and_oop/notes.md
@@ -0,0 +1,9 @@
+# Javascript and Classes
+
+## OOP
+## Object
+
+- collection of properties and methods 
+- toLowerCase
+
+# why use   OOP
---
+++ classes_and_oop/oop.js
@@ -0,0 +1,9 @@
+const user = {
+    username : " Mahendra",
+    loginCount :8,
+    signdIn : true
+
+}
+
+// console.log(user.username);
+console.log(user.loginCount);
