d7b0b986179abea4180aea37efddb6c018f49092
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-02-28 13:09:23 +0000

    dfg

--- classes_and_oop/oop.js
+++ classes_and_oop/oop.js
@@ -1,9 +1,36 @@
 const user = {
-    username : " Mahendra",
-    loginCount :8,
-    signdIn : true
+    username: "Mahendra",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`); // only username can't write 
+        console.log(this); // this print the all values in class
+    }
 
 }
 
-// console.log(user.username);
-console.log(user.loginCount);
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("hitesh", 12, true)
+const userTwo = new User("ChaiAurCode", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
