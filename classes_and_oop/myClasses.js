16f9c346ded5f68ac2457ec3e2958464ef3d54a0
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-01 13:00:21 +0000

    write the comments is this program

--- classes_and_oop/myClasses.js
+++ classes_and_oop/myClasses.js
@@ -1,34 +1,40 @@
-// ES6
-
+// Define a User class using ES6 class syntax
 class User {
+    // Constructor to initialize username, email, and password properties
     constructor( username , email, password){
         this.username = username ;
         this.email = email ;
         this.password = password
     }
 
+    // Method to encrypt the password by appending 'abc'
     encryptPassword(){
         return `${this.password}abc`
     }
 
+    // Method to change the username to uppercase
     changeUsername(){
         return `${this.username.toUpperCase()}`
     }
 }
 
+// Create a new User instance 'chai' with the provided values
 const chai = new User ( "Mahi" , "Mahendra@google.com" , "1234")
 
+// Log the result of calling encryptPassword and changeUsername on 'chai'
 console.log(chai.encryptPassword());
 console.log(chai.changeUsername());
 
-// behind the sence
+// Behind the scenes using constructor functions and prototypes
 
+// Define a User constructor function
 function User(username ,email ,password){
     this.username = username
     this.email = email
     this.password = password
 }
 
+// Add methods to the User prototype to encrypt the password and change the username
 User.prototype.encryptPassword = function(){
     return `${this.password}abc`
 }
@@ -37,7 +43,9 @@ User.prototype.changeUsername = function(){
     return `${this.username.toUpperCase()}`
 }
 
+// Create a new User instance 'tea' using the constructor function
 const tea  = new User (" tea", "Tea@gamil.com" ," 1234")
 
+// Log the result of calling encryptPassword and changeUsername on 'tea'
 console.log(tea.encryptPassword());
 console.log(tea.changeUsername());
