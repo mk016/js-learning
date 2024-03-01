8ecfcbf14a00945135615ca37fce80acd73737b5
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-01 12:59:04 +0000

    my class js compelete

--- classes_and_oop/myClasses.js
+++ classes_and_oop/myClasses.js
@@ -20,3 +20,24 @@ const chai = new User ( "Mahi" , "Mahendra@google.com" , "1234")
 
 console.log(chai.encryptPassword());
 console.log(chai.changeUsername());
+
+// behind the sence
+
+function User(username ,email ,password){
+    this.username = username
+    this.email = email
+    this.password = password
+}
+
+User.prototype.encryptPassword = function(){
+    return `${this.password}abc`
+}
+
+User.prototype.changeUsername = function(){
+    return `${this.username.toUpperCase()}`
+}
+
+const tea  = new User (" tea", "Tea@gamil.com" ," 1234")
+
+console.log(tea.encryptPassword());
+console.log(tea.changeUsername());
