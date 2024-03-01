69c419f0396186a7711b631e74f594e67b9f8e35
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-01 12:49:13 +0000

    all program is wroking

---
+++ classes_and_oop/myClasses.js
@@ -0,0 +1,22 @@
+// ES6
+
+class User {
+    constructor( username , email, password){
+        this.username = username ;
+        this.email = email ;
+        this.password = password
+    }
+
+    encryptPassword(){
+        return `${this.password}abc`
+    }
+
+    changeUsername(){
+        return `${this.username.toUpperCase()}`
+    }
+}
+
+const chai = new User ( "Mahi" , "Mahendra@google.com" , "1234")
+
+console.log(chai.encryptPassword());
+console.log(chai.changeUsername());
