debc291d0a38d49a4ee29e3b0a9b3d93259e9857
Mahendra Kumawat <117855423+mk016@users.noreply.github.com>
2024-03-02 19:31:49 +0000

    some details add

--- classes_and_oop/Mathpi.js
+++ classes_and_oop/Mathpi.js
@@ -1,23 +1,45 @@
-const descripter = Object.getOwnPropertyDescriptor(Math ,"PI")
+// Get the property descriptor of the Math object's property "PI"
+const descriptor = Object.getOwnPropertyDescriptor(Math, "PI");
 
-console.log(descripter);
+// Log the property descriptor to the console
+// console.log(descriptor);
 
-// console.log(Math.PI)
+// Log the value of Math.PI to the console
+// console.log(Math.PI);
+
+// Attempt to assign a new value to Math.PI
 // Math.PI = 5;
-// console.log(Math.PI)
 
-const chai ={
-    name : 'tulsi chai ',
-    price : 250,
-    isAvailable :true
+// Log the new value of Math.PI to the console
+// console.log(Math.PI);
 
+// Define an object named chai representing information about ginger chai
+const chai = {
+    name: 'ginger chai', // Name of the chai
+    price: 250, // Price of the chai
+    isAvailable: true, // Availability status of the chai
+    orderChai: function(){ // Method to order the chai
+        console.log("chai nhi bni"); // Log a message indicating chai is not prepared
+    }
 }
 
-//console.log(chai)
-console.log(Object.getOwnPropertyDescriptor(chai ,"name"));
+// Get the property descriptor of the "name" property of the chai object
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
 
+// Define a new property descriptor for the "name" property of the chai object
+Object.defineProperty(chai, 'name', {
+    // writable: false, // Property is not writable (commented out to avoid error)
+    enumerable: true, // Property is enumerable
+});
 
-Object.defineProperty(chai , "name",{
-    Writable :false,
-    enaumerable : false
-})
+// Get the updated property descriptor of the "name" property of the chai object
+console.log(Object.getOwnPropertyDescriptor(chai, "name"));
+
+// Iterate over the properties of the chai object
+for (let [key, value] of Object.entries(chai)) {
+    // Check if the value is not a function
+    if (typeof value !== 'function') {
+        // Log the key-value pair to the console
+        console.log(`${key} : ${value}`);
+    }
+} 
